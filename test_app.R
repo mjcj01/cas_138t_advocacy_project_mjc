dfb6b3dbb141b5a4ece73b3920919f80b7955ab2
Michael Cattell <86273177+mjcj01@users.noreply.github.com>
2022-04-09 18:26:26 -0400

    Update test_app.R

--- test_app.R
+++ test_app.R
@@ -1,9 +1,10 @@
 library(shiny)
 library(tidyverse)
 library(plotly)
+library(rsconnect)
 
 ui <- fluidPage(
-  titlePanel(h2("School District Expenditures per Student")),
+  titlePanel("School District Expenditures per Student"),
   sidebarLayout(
     sidebarPanel(
       
@@ -11,12 +12,14 @@ ui <- fluidPage(
                   label = NULL,
                   choices = c(afr_expenditures_all$School.District),
                   selected = NULL),
-      checkboxInput("state_avg", "Show State Average Line:", FALSE)),
+      checkboxInput("state_avg", "Show State Average Line", FALSE),
+      ),
     
     mainPanel(
-      conditionalPanel("input.state_avg == true", plotlyOutput("districtspending_withstate")),
-      conditionalPanel("input.state_avg == false", plotlyOutput("districtspending_nostate")),
-      verbatimTextOutput(FALSE)
+      conditionalPanel("input.state_avg == true", plotlyOutput("districtspending_withstate", height = "600px")),
+      conditionalPanel("input.state_avg == false", plotlyOutput("districtspending_nostate", height = "600px")),
+      verbatimTextOutput(FALSE),
+      
     )
   )
 )
@@ -25,16 +28,20 @@ server <- function(input, output) {
   schooldistrictInput <- reactive({
     afr_expenditures_all %>% filter(School.District == input$schooldistrict)
   })
-
+  
   output$districtspending_nostate <- renderPlotly({
     validate(
       need(input$schooldistrict, "Please select a school district")
     )
     
     ggplot(data = schooldistrictInput(), aes(x = Year)) +
-      geom_line(aes(y = Total.Exp.per.ADM)) +
+      geom_line(aes(y = Total.Exp.per.ADM), color = "#0AB6FF", size = 1.5) +
+      geom_point(aes(y = Total.Exp.per.ADM), color = "#0AB6FF", size = 3.5) +
       labs(x = "Year", y = "Spending per Student (in USD)") +
-      scale_x_continuous(breaks=c(2012,2014,2016,2018,2020))
+      scale_x_continuous(breaks=c(2012,2014,2016,2018,2020)) +
+      theme(
+        axis.title = element_text(face = "bold")
+      )
   })
   
   output$districtspending_withstate <- renderPlotly({
@@ -43,14 +50,17 @@ server <- function(input, output) {
     )
     
     ggplot(data = schooldistrictInput(), aes(x = Year)) +
-      geom_line(aes(y = Total.Exp.per.ADM)) +
-      geom_line(aes(y = state_average_2011_2020$Total.Exp.per.ADM)) +
+      geom_line(aes(y = Total.Exp.per.ADM), color = "#0AB6FF", size = 1.5) +
+      geom_point(aes(y = Total.Exp.per.ADM), color = "#0AB6FF", size = 3.5) +
+      geom_line(aes(y = state_average_2011_2020$Total.Exp.per.ADM), size = 1.5) +
+      geom_point(aes(y = state_average_2011_2020$Total.Exp.per.ADM), size = 3.5) +
       labs(x = "Year", y = "Spending per Student (in USD)") +
-      scale_x_continuous(breaks=c(2012,2014,2016,2018,2020))
+      scale_x_continuous(breaks=c(2012,2014,2016,2018,2020)) +
+      theme(
+        axis.title = element_text(face = "bold")
+      )
   })
   
 }
 
 shinyApp(ui = ui, server = server)
-
-
