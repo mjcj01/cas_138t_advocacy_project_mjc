1752210541bb37a4a871559b1a17fe081dc2231c
Michael Cattell <86273177+mjcj01@users.noreply.github.com>
2022-04-09 10:11:41 -0400

    Update app.R

--- app.R
+++ app.R
@@ -10,10 +10,13 @@ ui <- fluidPage(
       selectInput(inputId = "schooldistrict",
                   label = NULL,
                   choices = c(afr_expenditures_all$School.District),
-                  selected = NULL)),
+                  selected = NULL),
+      checkboxInput("state_avg", "Show State Average Line:", FALSE)),
     
     mainPanel(
-      plotlyOutput("districtspending")
+      conditionalPanel("input.state_avg == true", plotlyOutput("districtspending_withstate")),
+      conditionalPanel("input.state_avg == false", plotlyOutput("districtspending_nostate")),
+      verbatimTextOutput(FALSE)
     )
   )
 )
@@ -22,20 +25,32 @@ server <- function(input, output) {
   schooldistrictInput <- reactive({
     afr_expenditures_all %>% filter(School.District == input$schooldistrict)
   })
-  
-  output$districtspending <- renderPlotly({
-    
+
+  output$districtspending_nostate <- renderPlotly({
     validate(
       need(input$schooldistrict, "Please select a school district")
     )
     
-    ggplot(data= schooldistrictInput(), aes(x = Year)) +
+    ggplot(data = schooldistrictInput(), aes(x = Year)) +
       geom_line(aes(y = Total.Exp.per.ADM)) +
       labs(x = "Year", y = "Spending per Student (in USD)") +
       scale_x_continuous(breaks=c(2012,2014,2016,2018,2020))
-    
   })
+  
+  output$districtspending_withstate <- renderPlotly({
+    validate(
+      need(input$schooldistrict, "Please select a school district")
+    )
+    
+    ggplot(data = schooldistrictInput(), aes(x = Year)) +
+      geom_line(aes(y = Total.Exp.per.ADM)) +
+      geom_line(aes(y = state_average_2011_2020$Total.Exp.per.ADM)) +
+      labs(x = "Year", y = "Spending per Student (in USD)") +
+      scale_x_continuous(breaks=c(2012,2014,2016,2018,2020))
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
+
 
