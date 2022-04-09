6a4aa4ad6fd919f64eb64d11e73404f3aaab70b9
Michael Cattell <86273177+mjcj01@users.noreply.github.com>
2022-04-09 10:01:27 -0400

    Update app.R

--- app.R
+++ app.R
@@ -8,9 +8,9 @@ ui <- fluidPage(
     sidebarPanel(
       
       selectInput(inputId = "schooldistrict",
-                  label = "Choose a School District",
+                  label = NULL,
                   choices = c(afr_expenditures_all$School.District),
-                  selected = c("Bermudian Springs SD"))),
+                  selected = NULL)),
     
     mainPanel(
       plotlyOutput("districtspending")
@@ -19,22 +19,22 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
- schooldistrictInput <- reactive({
-   afr_expenditures_all %>% filter(School.District == input$schooldistrict)
- })
- 
- output$districtspending <- renderPlotly({
-   
-   validate(
-     need(input$schooldistrict, "Please select a school district")
-   )
-   
-   ggplot(data= schooldistrictInput(), aes(x = Year, y = Total.Exp.per.ADM)) +
-     geom_line() +
-     labs(x = "Year", y = "Spending per Student (in USD)") +
-     scale_x_continuous(breaks=c(2012,2014,2016,2018,2020))
+  schooldistrictInput <- reactive({
+    afr_expenditures_all %>% filter(School.District == input$schooldistrict)
+  })
   
-   })
+  output$districtspending <- renderPlotly({
+    
+    validate(
+      need(input$schooldistrict, "Please select a school district")
+    )
+    
+    ggplot(data= schooldistrictInput(), aes(x = Year)) +
+      geom_line(aes(y = Total.Exp.per.ADM)) +
+      labs(x = "Year", y = "Spending per Student (in USD)") +
+      scale_x_continuous(breaks=c(2012,2014,2016,2018,2020))
+    
+  })
 }
 
 shinyApp(ui = ui, server = server)
