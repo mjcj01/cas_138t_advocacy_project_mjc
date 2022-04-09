ca60ab90e0c24ba3a9a15bf9d6c452738249ed4d
Michael Cattell <86273177+mjcj01@users.noreply.github.com>
2022-04-09 18:28:30 -0400

    Update graveyard.R

--- graveyard.R
+++ graveyard.R
@@ -1,6 +1,19 @@
+### Old plot code
+
 schooldistrict <- schooldistrictInput()
 ggplot(schooldistrict, aes(x = FullDate, y = Total.Exp.per.ADM)) +
   geom_line() +
   geom_label(aes(label = Total.Exp.per.ADM)) +
   labs(x = "Year", y = "Spending per Student (in USD)") +
   scale_x_date(date_labels = "%Y", limits = as.Date(c("2010-1-1", "2021-1-1")))
+
+### Date combination code
+
+afr_expenditures_all$FullDate <- as.Date(with(afr_expenditures_all, paste(Year, Month, Day, sep = "-")), "%Y-%m-%d")
+
+### Testing ways to implement the state average line
+
+test <- afr_expenditures_all %>%
+  filter(County == "Adams")
+
+
