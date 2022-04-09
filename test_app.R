435820b11c56f2a3426e5c817fa15b03a2d3880d
Michael Cattell <86273177+mjcj01@users.noreply.github.com>
2022-04-09 10:01:48 -0400

    Add files via upload

---
+++ test_app.R
@@ -0,0 +1,47 @@
+library(shiny)
+library(tidyverse)
+library(plotly)
+
+ui <- fluidPage(
+  titlePanel(h2("School District Expenditures per Student")),
+  sidebarLayout(
+    sidebarPanel(
+      
+      selectInput(inputId = "schooldistrict",
+                  label = NULL,
+                  choices = c(afr_expenditures_all$School.District),
+                  selected = NULL),
+      checkboxInput("state_avg", "Show State Average Line:", FALSE)),
+    
+    mainPanel(
+      plotlyOutput("districtspending"),
+      verbatimTextOutput(FALSE)
+    )
+  )
+)
+
+server <- function(input, output) {
+  schooldistrictInput <- reactive({
+    afr_expenditures_all %>% filter(School.District == input$schooldistrict)
+  })
+  
+  output$state_avg <- renderText({input$state_avg})
+  
+  output$districtspending <- renderPlotly({
+    
+    validate(
+      need(input$schooldistrict, "Please select a school district")
+    )
+    
+    p <- ggplot(data = schooldistrictInput(), aes(x = Year)) +
+      geom_line(aes(y = Total.Exp.per.ADM)) +
+      labs(x = "Year", y = "Spending per Student (in USD)") +
+      scale_x_continuous(breaks=c(2012,2014,2016,2018,2020))
+    
+    
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+
