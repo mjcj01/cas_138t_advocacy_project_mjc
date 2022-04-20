e6bb263ecc2ce6fbedd26266e3b74cd705d01ffb
Michael Cattell <86273177+mjcj01@users.noreply.github.com>
2022-04-19 21:48:44 -0400

    Update test_app.R

--- test_app.R
+++ test_app.R
@@ -8,7 +8,7 @@ library(rsconnect)
 
 ui <- fluidPage(
   
-  titlePanel("School District Expenditures per Student"),
+  titlePanel("School District Total Spending per Student"),
   
   sidebarLayout(
     
@@ -18,17 +18,17 @@ ui <- fluidPage(
                   label = NULL,
                   choices = c(afr_expenditures_all$`School District`),
                   selected = NULL),
-      checkboxInput("state_avg", "Show State Average Line", FALSE),
+      checkboxInput("state_median", "Show State Median Line", FALSE),
       HTML("<p>Data is sourced from the Pennsylvania Department of Education. Click 
-           <a href = https://www.education.pa.gov/Teachers%20-%20Administrators/School%20Finances/
-           Finances/AFR%20Data%20Summary/Pages/AFR-Data-Summary-Level.aspx>here</a> to access it!</p>"),
+           <a href = https://www.education.pa.gov/Teachers%20-%20Administrators/School%20Finances/Finances/AFR%20Data%20Summary/Pages/AFR-Data-Summary-Level.aspx>here</a> to access it!</p>"),
       HTML("<p><img src = blue_line.png height = 12.5 width = 25> is the selected school district's decade expenditures per student.</p>"),
-      HTML("<p><img src = black_line.png height = 12.5 width = 25> is the average expenditures across the state.</p>")
+      HTML("<p><img src = green_line.png height = 12.5 width = 25> is the median instructional expenditure across the state.</p>"),
+      HTML("<p>Total Spending per Student calculation includes instruction spending, as well as spending on facilities, support services, and other miscellaneous categories.")
     ),
     
     mainPanel(
-      conditionalPanel("input.state_avg == true", plotlyOutput("districtspending_withstate", height = "600px")),
-      conditionalPanel("input.state_avg == false", plotlyOutput("districtspending_nostate", height = "600px")),
+      conditionalPanel("input.state_median == true", plotlyOutput("districtspending_median", height = "600px")),
+      conditionalPanel("input.state_median == false", plotlyOutput("districtspending", height = "600px")),
       verbatimTextOutput(FALSE),
       
     )
@@ -41,15 +41,17 @@ server <- function(input, output) {
     afr_expenditures_all %>% filter(`School District` == input$schooldistrict)
   })
   
-  output$districtspending_nostate <- renderPlotly({
+  ### Showing district spending  
+  
+  output$districtspending <- renderPlotly({
     validate(
       need(input$schooldistrict, "Please select a school district")
     )
     
     ggplot(data = schooldistrictInput(), aes(x = Year)) +
-      geom_line(aes(y = `Expenditures per Student`), color = "#0AB6FF", size = 1.5) +
-      geom_point(aes(y = `Expenditures per Student`), color = "#0AB6FF", size = 3.5) +
-      labs(x = "Year", y = "Spending per Student (in USD)") +
+      geom_line(aes(y = `Instructional Expenditures per Student`), color = "#0AB6FF", size = 1.5) +
+      geom_point(aes(y = `Instructional Expenditures per Student`), color = "#0AB6FF", size = 3.5) +
+      labs(x = "Year", y = "Total Spending per Student (in USD)") +
       scale_x_continuous(breaks=c(2011,2012,2013,2014,2015,2016,2017,2018,2019,2020)) +
       theme(
         axis.title = element_text(face = "bold"),
@@ -57,23 +59,24 @@ server <- function(input, output) {
       )
   })
   
-  output$districtspending_withstate <- renderPlotly({
+  ### Showing district spending + median line
+  
+  output$districtspending_median <- renderPlotly({
     validate(
       need(input$schooldistrict, "Please select a school district")
     )
     
     ggplot(data = schooldistrictInput(), aes(x = Year)) +
-      geom_line(aes(y = `Expenditures per Student`), color = "#0AB6FF", size = 1.5) +
-      geom_point(aes(y = `Expenditures per Student`), color = "#0AB6FF", size = 3.5) +
-      geom_line(aes(y = `Average Expenditures per Student Across PA`), size = 1.5) +
-      geom_point(aes(y = `Average Expenditures per Student Across PA`), size = 3.5) +
-      labs(x = "Year", y = "Spending per Student (in USD)") +
+      geom_line(aes(y = `Instructional Expenditures per Student`), color = "#0AB6FF", size = 1.5) +
+      geom_point(aes(y = `Instructional Expenditures per Student`), color = "#0AB6FF", size = 3.5) +
+      geom_line(aes(y = `Median Expenditures per Student Across PA`), color = "#7BFF00", size = 1.5) +
+      geom_point(aes(y = `Median Expenditures per Student Across PA`), color = "#7BFF00", size = 3.5) +
+      labs(x = "Year", y = "Total Spending per Student (in USD)") +
       scale_x_continuous(breaks=c(2011,2012,2013,2014,2015,2016,2017,2018,2019,2020)) +
       theme(
         axis.title = element_text(face = "bold")
       )
   })
-  
 }
 
 shinyApp(ui = ui, server = server)
